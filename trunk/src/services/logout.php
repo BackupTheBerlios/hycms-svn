34dae2ba9393c26e55faa66c03bf61a1f0d41c7a
smeky <smeky@a3e5e67e-6865-0410-8142-b050a96042d3>
2009-04-24 10:01:51 +0000

    To check the Session management its nececery to log out

---
+++ trunk/src/services/logout.php
@@ -0,0 +1,14 @@
+<?php 
+	/*
+	 * hyCMS
+	 * Copyright(C)2009 by Peter Neubauer
+	 * Published under the terms of the Lesser GNU General Public License v2
+	 *
+	 */
+?>
+<?php
+	include "session.php";
+	
+	delete_session();
+	echo "Logged out";
+?>
